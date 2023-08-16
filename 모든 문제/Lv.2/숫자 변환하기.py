00b93b2afdb325e06f1ec4c798521e51e6bb5d29
Han JinSeo <jinseo0278@gmail.com>
2023-08-16 17:27:04 +0900

    Add Problem

---
+++ 모든 문제/Lv.2/숫자 변환하기.py
@@ -0,0 +1,39 @@
+'''
+문제 설명
+자연수 x를 y로 변환하려고 합니다. 사용할 수 있는 연산은 다음과 같습니다.
+x에 n을 더합니다
+x에 2를 곱합니다.
+x에 3을 곱합니다.
+자연수 x, y, n이 매개변수로 주어질 때, x를 y로 변환하기 위해 필요한 최소 연산 횟수를 return하도록 solution 함수를 완성해주세요. 
+이때 x를 y로 만들 수 없다면 -1을 return 해주세요.
+
+제한사항
+1 ≤ x ≤ y ≤ 1,000,000
+1 ≤ n < y
+'''
+
+def solution(x, y, n):
+    answer = 0
+    s = set()
+    s.add(x)
+
+    while s:
+        if y in s:
+            return answer
+        nxt = set()
+        for i in s:
+            if i+n <= y:
+                nxt.add(i+n)
+            if i*2 <= y:
+                nxt.add(i*2)
+            if i*3 <= y:
+                nxt.add(i*3)
+        s = nxt
+        answer+=1
+
+    return -1
+
+x = 10
+y = 40
+n = 5
+solution(x, y, n)   # 2
